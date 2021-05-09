163d89cff98af538a41fe833a7b481f2cb56fc54
Kirsty Chatterton <chattertonkirsty@gmail.com>
2021-05-09 17:15:26 +0100

    style: refactor products page and remove sort by buttons

--- products/views.py
+++ products/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
-from django.db.models.functions import Lower
 
 from .models import Product, Category
 
@@ -14,24 +13,9 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
-    sort = None
-    direction = None
 
     # Filter/Search Queries
     if request.GET:
-        # Sort By
-        if 'sort' in request.GET:
-            sortkey = request.GET['sort']
-            sort = sortkey
-            if sortkey == 'name':
-                sortkey = 'lower_name'
-                products = products.annotate(lower_name=Lower('name'))
-            if 'direction' in request.GET:
-                direction = request.GET['direction']
-                if direction == 'desc':
-                    sortkey = f'-{sortkey}'
-            products = products.order_by(sortkey)
-
         # Filter Products by Category
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
@@ -50,13 +34,10 @@ def all_products(request):
                 description__icontains=query)
             products = products.filter(queries)
 
-    current_sorting = f'{sort}_{direction}'
-
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
-        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
