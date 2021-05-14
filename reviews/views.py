fd606f65daa52018981223ce3bc18985e1499ae1
Kirsty Chatterton <chattertonkirsty@gmail.com>
2021-05-14 22:34:44 +0100

    add reviews template and urls to reviews app

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+from .models import Review
 
 # Create your views here.
+
+
+def all_reviews(request):
+    # A view to display all reviews
+    reviews = Review.objects.all()
+
+    context = {
+        'reviews': reviews,
+    }
+
+    return render(request, 'reviews/reviews.html', context)
+
+
+@login_required()
+def add_review(request):
+    # Add review to site
+
+    return render(request)
--- the_wee_norn_iron_box/urls.py
+++ the_wee_norn_iron_box/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('box/', include('box.urls')),
+    path('reviews/', include('reviews.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
