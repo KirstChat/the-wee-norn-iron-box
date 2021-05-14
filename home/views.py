1c51e51e2feaadd4683eb2f9588896f827bd835e
Kirsty Chatterton <chattertonkirsty@gmail.com>
2021-05-14 18:20:12 +0100

    display test review on index.html in home app

--- home/views.py
+++ home/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
+from reviews.models import Review
+
 # Create your views here.
 
 
 def index(request):
     # A view to return the index page
-    return render(request, 'home/index.html')
+    reviews = Review.objects.all()
+
+    context = {
+        'reviews': reviews,
+    }
+
+    return render(request, 'home/index.html', context)
--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from.models import Category, Product
+from .models import Category, Product
 
 # Register your models here.
 admin.site.register(Category)
-# admin.site.register(Product)
 
 
 @admin.register(Product)
