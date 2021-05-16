f61c640780188c3066b63f5dfcc5b6d3fd20d05f
Kirsty Chatterton <chattertonkirsty@gmail.com>
2021-05-16 21:31:59 +0100

    add forms.py to reviews app

--- reviews/models.py
+++ reviews/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.fields.related import OneToOneField
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
@@ -12,8 +13,10 @@ class Review(models.Model):
 
     user = OneToOneField(User, on_delete=models.CASCADE)
     review = models.TextField(null=True, blank=True)
-    dated_added = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField()
+    dated_added = models.DateField(auto_now_add=True)
+    rating = models.IntegerField(validators=[
+        MinValueValidator(1),
+        MaxValueValidator(5)])
 
     def __str__(self):
         return self.review
