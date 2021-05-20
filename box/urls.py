b77457e5eca070339d82c6019b5841b2893fdf85
Kirsty Chatterton <chattertonkirsty@gmail.com>
2021-05-20 22:03:22 +0100

    add remove item from box functionality

--- box/urls.py
+++ box/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.view_box, name='view_box'),
     path('add/<item_id>', views.add_to_box, name='add_to_box'),
+    path('remove/<item_id>', views.remove_from_box, name='remove_from_box'),
 ]
--- box/views.py
+++ box/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -13,7 +13,7 @@ def view_box(request):
 
 @login_required
 def add_to_box(request, item_id):
-    # Add quantity of specified product
+    # Add product to box
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -28,3 +28,16 @@ def add_to_box(request, item_id):
     request.session['box'] = box
     print(request.session['box'])
     return redirect(redirect_url)
+
+
+@login_required
+def remove_from_box(request, item_id):
+    # Remove product from box
+
+    box = request.session.get('box', {})
+
+    if item_id in box:
+        box.pop(item_id)
+
+    request.session['box'] = box
+    return redirect(reverse('view_box'))
