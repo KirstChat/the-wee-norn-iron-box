8e2f4a69c2f5aa5bf384494810df4d4825f71996
Kirsty Chatterton <chattertonkirsty@gmail.com>
2021-06-07 19:54:54 +0100

    update checkout page layout and add stripe_elements.js

--- checkout/views.py
+++ checkout/views.py
@@ -17,6 +17,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51ITBQLAMYLVibdjZUpRFyT5zA5ZEaidGAc7LkAhnNe8jGoZaYsjg2BeVfstKvGPBuiGmprXizchISIZlqYRqMbtp00pg9y5UdT',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
