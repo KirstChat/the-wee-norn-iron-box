4cd1e9756a1b975fb8553bb467ff5d38c3252feb
Kirsty Chatterton <chattertonkirsty@gmail.com>
2021-06-10 18:51:09 +0100

    fix: setup basic payment system in place of subscription system

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,7 +1,7 @@
 /* CSS and JS from https://stripe.com/docs/js */
 
-let stripePublicKey = document.getElementById("id_stripe_public_key").textContent.slice(1, -1);
-let clientSecret = document.getElementById("id_client_secret").textContent.slice(1, -1);
+let stripePublicKey = document.getElementById('id_stripe_public_key').textContent.slice(1, -1);
+let clientSecret = document.getElementById('id_client_secret').textContent.slice(1, -1);
 let stripe = Stripe(stripePublicKey);
 let elements = stripe.elements();
 
@@ -22,19 +22,53 @@ let style = {
     }
 };
 
-let cardElement = elements.create("card", {
+let cardElement = elements.create('card', {
     style: style 
 });
 
 cardElement.mount('#card-element');
 
 cardElement.on('change', function (event) {
-    let cardElementError = document.getElementById("card-element-error");
+    let cardElementError = document.getElementById('card-element-error');
     if (event.error) {
         cardElementError.innerHTML = `
             <span><i class="fas fa-exclamation-circle"></i></span>
-            <span>${event.error.message}</span>`
+            <span>${event.error.message}</span>`;
     } else {
         cardElementError.textContent = " ";
     }
+})
+
+// Handle Form Submit
+let form = document.getElementById('payment-form');
+form.addEventListener('submit', function (event) {
+    // Prevent default action of POST
+    event.preventDefault();
+    // Disable card element and submit button to prevent multiple submissions
+    card.update({
+        'disabled': true
+    })
+    document.getElementById('submit-button').setAttribute('disabled', true)
+    stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: cardElement,
+            },
+        })
+        .then(function (result) {
+            if (result.error) {
+                let cardElementError = document.getElementById
+                cardElementError.innerHTML = `
+                    <span><i class="fas fa-exclamation-circle"></i></span>
+                    <span>${result.error.message}</span>`;
+                // Re-enable card element and submit button to allow user to re-enter details
+                card.update({
+                    'disabled': false
+                })
+                document.getElementById('submit-button').setAttribute('disabled', false)
+            } else {
+                if (result.paymentIntent.status === 'succeeded') {
+                    form.submit();
+                }
+            }
+        });
 })
--- checkout/views.py
+++ checkout/views.py
@@ -10,18 +10,31 @@ import stripe
 
 
 def checkout(request):
-    box = request.session.get('box', {})
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
 
+    box = request.session.get('box', {})
     if not box:
         messages.error(request, 'You haven\'t added anything to your wee box!')
         return redirect(reverse('products'))
+
+    stripe_total = settings.STANDARD_PRICE
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing! \
+            Did you forget to set it in you environment?')
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51ITBQLAMYLVibdjZUpRFyT5zA5ZEaidGAc7LkAhnNe8jGoZaYsjg2BeVfstKvGPBuiGmprXizchISIZlqYRqMbtp00pg9y5UdT',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
